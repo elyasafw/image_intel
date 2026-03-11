f1dfa7d7dc4f284faabd3614c166635dd0a92982
Avi Gross <avigross2018@gmail.com>
2026-03-11 13:08:23 +0200

    added the drag and drop feature

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request
 import os
-
-
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -11,12 +13,27 @@ def index():
 
 @app.route('/analyze', methods=['POST'])
 def analyze_images():
-    folder_path = request.form.get('folder_path')
+    files = request.files.getlist('images')
     
+    if not files or files[0].filename == '':
+        return render_template('index.html', error="לא נבחרו קבצים")
+
+    folder_path = app.config['UPLOAD_FOLDER']
+    
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+    
+    for file in files:
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(folder_path, filename))
+
     from extractor import extract_all
     images_data = extract_all(folder_path)
     if type(images_data) != list:
-        return render_template('index.html', error = images_data)
+        return render_template('index.html', error=images_data)
     
     from map_view import create_map
     map_html = create_map(images_data)
